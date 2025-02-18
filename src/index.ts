ea8c249f4742d56dbb5dfbd23f06d4deee436a99
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-02-18 17:18:26 +0200

    last update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-const port = process.env.PORT || 3000; // Default port to listen
 
-export const app = express(); // Create an express application
+const app = express();
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-
-}); // Define a route handler for the default home page
-app.listen(port, () => { 
-    console.log(`Server is running on http://localhost:${port}`);
+  res.send("Hello from TypeScript Express!");
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+export default app;
