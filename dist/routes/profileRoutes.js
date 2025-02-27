94e14ac42b0b25192364eb114c54c8491ed2b046
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-02-27 22:56:41 +0200

    add vercel file

--- dist/routes/profileRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import profileController from '../controllers/profileController.js';
-const router = Router();
-// Use the imported controller instance directly
-router.post('/', profileController.createProfile);
-export default router;
