e97f1c78d0e6773cd79a77b8644680189dc87e68
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-02-27 22:56:41 +0200

    add vercel file

--- dist/routes/skillsRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import SkillsController from "../controllers/skillsController.js";
-const router = Router();
-// Define routes for skills
-router.get("/", SkillsController.getSkills);
-export default router;
