0d719928f9d47b5daa083342fd4da909b367ad76
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-02-28 00:41:06 +0200

    test husky and lint-staged with pre prettier

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,23 @@ app.use("/api/upload", UploadRouter);
 
 // Start the Express server
 const port = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
