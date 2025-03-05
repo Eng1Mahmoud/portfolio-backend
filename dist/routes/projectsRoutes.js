8a275191e6724cf98b211d8f2bef53cc6c5fab97
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-03-05 21:31:50 +0200

    add projects , functions (add, edit , delete, git ) and add routes , modify model

--- dist/routes/projectsRoutes.js
+++ dist/routes/projectsRoutes.js
@@ -1,8 +1,24 @@
-import { Router } from 'express';
-import ProjectsController from '../controllers/projectsController.js';
+import { Router } from "express";
+import ProjectsController from "../controllers/projectsController.js";
 const router = Router();
-// Define routes for projects
-router.get('/', (req, res) => {
+// create new project
+router.post("/", (req, res) => {
+    ProjectsController.createProject(req, res);
+});
+// update project
+router.put("/:id", (req, res) => {
+    ProjectsController.updateProject(req, res);
+});
+// delete project
+router.delete("/:id", (req, res) => {
+    ProjectsController.deleteProject(req, res);
+});
+// get project by id
+router.get("/:id", (req, res) => {
+    ProjectsController.getProjectById(req, res);
+});
+// get all projects
+router.get("/", (req, res) => {
     ProjectsController.getProjects(req, res);
 });
 export default router;
--- src/routes/projectsRoutes.ts
+++ src/routes/projectsRoutes.ts
@@ -1,11 +1,26 @@
-import { Request, Response, Router } from 'express';
-import ProjectsController from '../controllers/projectsController.js';
+import { Request, Response, Router } from "express";
+import ProjectsController from "../controllers/projectsController.js";
 
 const router = Router();
-// Define routes for projects
-router.get('/', (req:Request, res:Response) => {
-    ProjectsController.getProjects(req, res);
+// create new project
+router.post("/", (req: Request, res: Response) => {
+  ProjectsController.createProject(req, res);
 });
-
+// update project
+router.put("/:id", (req: Request, res: Response) => {
+  ProjectsController.updateProject(req, res);
+});
+// delete project
+router.delete("/:id", (req: Request, res: Response) => {
+  ProjectsController.deleteProject(req, res);
+});
+// get project by id
+router.get("/:id", (req: Request, res: Response) => {
+  ProjectsController.getProjectById(req, res);
+});
+// get all projects
+router.get("/", (req: Request, res: Response) => {
+  ProjectsController.getProjects(req, res);
+});
 
 export default router;
