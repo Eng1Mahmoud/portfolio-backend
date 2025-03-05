41b061b59f236857de5d01d24626c48090b7c0e1
Eng1Mahmoud <mahmoudabbamalik@gmail.com>
2025-03-05 21:55:31 +0200

    add adreess , github and linkedIn fileds to profile model

--- dist/models/Profile.js
+++ dist/models/Profile.js
@@ -3,12 +3,15 @@ const ProfileSchema = new Schema({
     userName: { type: String, required: true },
     title: { type: String, required: true },
     email: { type: String, required: true },
+    address: { type: String, required: true },
     phone1: { type: String, required: true },
     phone2: { type: String, required: false },
     bio: { type: String, required: true },
     avatar: { type: String, required: true },
     aboutImage: { type: String, required: true },
     cv: { type: String, required: true },
+    github: { type: String, required: true },
+    linkedin: { type: String, required: true },
 });
 const Profile = model('Profile', ProfileSchema);
 export default Profile;
